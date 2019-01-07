e84692cc94bec267d311d538f3b6fa7c87340b82
aishwarysovani <aishusovani@gmail.com>
2019-01-07 11:42:11 +0530

    update1

--- com/bridgelabz/datastructure/Calendar.java
+++ com/bridgelabz/datastructure/Calendar.java
@@ -12,12 +12,9 @@ public class Calendar {
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		   int month;
-		   int year;
-	       Scanner scan = new Scanner(System.in);
-           System.out.println("Enter month and year");
-           month=scan.nextInt();
-           year=scan.nextInt();
+		   int month = Integer.parseInt(args[0]);
+		   int year = Integer.parseInt(args[1]);
+          
            //find day at first date of month 
            int startDay = Utility.findDcode(1, month, year);
            //print calendar of month
